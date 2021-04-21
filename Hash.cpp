c47634e350e272102fe73bb417b3663b0088d1ff
lydia-smith106 <78553746+lydia-smith106@users.noreply.github.com>
2021-04-21 12:28:34 -0600

    Update Hash.cpp

--- Hash.cpp
+++ Hash.cpp
@@ -11,7 +11,7 @@ using namespace std;
 template<class T>
 class Hash
 {
-    const static int HASHSIZE = 140000;
+    const static int HASHSIZE = 153337;
     int size;
     int found_compares;
     int not_found_compares;
